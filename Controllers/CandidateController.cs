7f5040cd3107a06b9c86932a95d2941700e81801
Apo Mene <apomene@yahoo.gr>
2025-03-20 13:14:24 +0200

    Add EntityFramework and DbCOntect class to model. Implemente CandidateController

--- Controllers/CandidateController.cs
+++ Controllers/CandidateController.cs
@@ -1,11 +1,46 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using static ResumeApp.Model.ResumeModel;
+using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace ResumeApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CandidateController : ControllerBase
+    public class CandidatesController : ControllerBase
     {
+        private readonly AppDbContext _db;
+        public CandidatesController(AppDbContext db) => _db = db;
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCandidate(Candidate candidate)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            candidate.CreationTime = DateTime.UtcNow;
+            _db.Candidates.Add(candidate);
+            await _db.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetCandidate), new { id = candidate.Id }, candidate);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCandidates() => Ok(await _db.Candidates.ToListAsync());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCandidate(int id)
+        {
+            var candidate = await _db.Candidates.FindAsync(id);
+            return candidate == null ? NotFound() : Ok(candidate);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCandidate(int id)
+        {
+            var candidate = await _db.Candidates.FindAsync(id);
+            if (candidate == null) return NotFound();
+            _db.Candidates.Remove(candidate);
+            await _db.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
--- Model/ResumeModel.cs
+++ Model/ResumeModel.cs
@@ -1,10 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
 
 namespace ResumeApp.Model
 {
     public class ResumeModel
     {
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+            public DbSet<Candidate> Candidates => Set<Candidate>();
+            public DbSet<Degree> Degrees => Set<Degree>();
+        }
         public class Candidate
         {
             public int Id { get; set; }
