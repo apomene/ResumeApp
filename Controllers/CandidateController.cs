c70cf714ad9b4953d782051be11aaef4d5221e73
Apo Mene <apomene@yahoo.gr>
2025-03-20 11:46:31 +0200

    Add Controllers

---
+++ Controllers/CandidateController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ResumeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+    }
+}
